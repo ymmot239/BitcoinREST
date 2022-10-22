5685e5b1250274dafef96b42c56683ff1af5ec59
wes reitz <wesreitz@gmail.com>
2022-10-22 03:20:25 -0500

    added wes

--- maven-example-jar/src/main/java/com/journaldev/maven/classes/App.java
+++ maven-example-jar/src/main/java/com/journaldev/maven/classes/App.java
@@ -18,7 +18,7 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World tommy!" );
         ObjectMapper objectMapper = new ObjectMapper();
         String s = "";
         try {
